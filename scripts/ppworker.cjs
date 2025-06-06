e55050d2bdc00e10ecc98fe930819dfbc2a0166b
akcoo1 <56815972+akcoo1@users.noreply.github.com>
2025-06-06 12:58:34 +0800

    Update ppworker.cjs

--- scripts/ppworker.cjs
+++ scripts/ppworker.cjs
@@ -157,7 +157,7 @@ const updateWebEnv = async (androidResDir, webUrl, debug) => {
         // Replace the web URL in the loadUrl call
         let updatedContent = content.replace(
             /webView\.loadUrl\(".*?"\)/,
-            `webView.loadUrl("https://www.baidu.com/")`
+            `webView.loadUrl("${webUrl}")`
         )
         // if debug is true, add debug mode
         console.log('webview debug to:', debug)
